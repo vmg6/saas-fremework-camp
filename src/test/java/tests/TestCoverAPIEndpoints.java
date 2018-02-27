59c95189c977185c4a87a83ecba4cc33dd1530e1
vladimir_matviichenko <v.matviichenko@epom.com>
2018-02-27 23:37:48 +0200

    Test Create Contact

--- src/test/java/tests/TestCoverAPIEndpoints.java
+++ src/test/java/tests/TestCoverAPIEndpoints.java
@@ -16,7 +16,7 @@ public class TestCoverAPIEndpoints extends TestBaseTNG{
     private static final String FIRST_NAME = "Homer";
     private static final String LAST_NAME = "Simpson";
     private static final String EMAIL = FIRST_NAME + "." + LAST_NAME + "@gmail.com";
-    private static Integer USER_ID;
+    private static final String ENDPOINTS_SOURCE = "http://host:port/api/v1/contacts/";
 
     @Inject
     private APIEndpoints apiEndpoints;
@@ -27,30 +27,38 @@ public class TestCoverAPIEndpoints extends TestBaseTNG{
     }
 
     @Test
-    public void testCRUDOperation() {
+    public void testCreateContact() {
         User user = new User(FIRST_NAME, LAST_NAME);
+        Integer userId = apiEndpoints.createContact(user.getCreateBodyRequest())
+                .statusCode(201)
+                .extract().jsonPath()
+                .getInt("data.id[0]");
 
-        USER_ID = apiEndpoints.createContact(user.getCreateBodyRequest()).extract().jsonPath().getInt("data.id[0]");
-
-        apiEndpoints.getContactById(USER_ID)
+        apiEndpoints.getContactById(userId)
                 .statusCode(200)
-                .body("data.id[0]", is(USER_ID))
+                .body("data.id[0]", is(userId))
                 .body("data.info.email[0]", is(EMAIL))
                 .body("data.info.firstName[0]", is(FIRST_NAME))
-                .body("data.info.lastName[0]", is(LAST_NAME));
+                .body("data.info.lastName[0]", is(LAST_NAME))
+                .body("data.refs.patch[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.get[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.delete[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.put[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("status", is(200));
 
-        apiEndpoints.findContact(FIRST_NAME, EMAIL)
+        apiEndpoints.deleteContact(userId)
                 .statusCode(200)
+                .body("data.id[0]", is(userId))
                 .body("data.info.email[0]", is(EMAIL))
                 .body("data.info.firstName[0]", is(FIRST_NAME))
-                .body("data.info.lastName[0]", is(LAST_NAME));
+                .body("data.info.lastName[0]", is(LAST_NAME))
+                .body("data.refs.patch[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.get[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.delete[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("data.refs.put[0]", is(ENDPOINTS_SOURCE + userId))
+                .body("status", is(200));
 
-        user.setFirstName("Bart");
-        user.setLastName("Simpson");
-        apiEndpoints.updateContact(user.getCreateBodyRequest(), USER_ID)
-                .statusCode(200);
-
-        apiEndpoints.deleteContact(USER_ID).statusCode(200);
-
+        apiEndpoints.getContactById(userId).statusCode(404);
     }
+
 }
